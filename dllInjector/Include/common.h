c3ea20e3e437dfdc741cf0f7d06adff926bf8044
Ö <114071799+r0keb@users.noreply.github.com>
2024-07-29 14:59:21 +0200

    Delete dllInjector directory

--- dllInjector/Include/common.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <stdio.h>
-#include <Windows.h>
-#include <tlhelp32.h>
-
-BOOL DllInjection(HANDLE hProcess, LPCWSTR pDllName);
-BOOL EnumProcesses(IN LPCWSTR pProcName, OUT DWORD* dwPid, OUT HANDLE* hProcess);
